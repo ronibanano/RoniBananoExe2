6534ff4676e841ce07ff89769d67d1340ce07a91
מיטל <ronni.ban@sisma.org.il>
2024-01-02 15:32:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ronibananoexe2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ronibananoexe2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bt;
+    int count =0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt=findViewById(R.id.button);
+    }
+
+    public void exe(View view) {
+        count++;
+        bt.setText("this is a click number: "+count);
+    }
+}
